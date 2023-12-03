f05be40c3b93bf43d306d5a84d0cb926c89b17fd
muhammadisa226 <muhammadisa226@gmail.com>
2023-12-03 21:25:26 +0700

    all done

--- src/layouts/Home/index.jsx
+++ src/layouts/Home/index.jsx
@@ -18,7 +18,7 @@ const Home = () => {
       <HeroList heroImage1={HeroImage1} heroImage2={HeroImage2} />
       <div className="absolute top-0 left-0 w-full h-screen bg-light/50 dark:bg-dark/50">
         <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center ">
-          <h1 className="-mt-32 text-4xl font-bold text-dark sm:text-5xl dark:text-light font-pacifico md:-mt-0">
+          <h1 className="-mt-32 text-4xl font-bold text-primary_light sm:text-5xl dark:text-primary_dark font-pacifico md:-mt-0">
             {"I'm Muhammad Isa"}
           </h1>
           <h2 className="flex pt-4 text-2xl text-dark sm:text-3xl dark:text-light font-monserrat">
