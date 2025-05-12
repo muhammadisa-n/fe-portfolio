c9b197de1567c50d99e4c4209f6c796d11ec3f1c
muhammadisa-n <muhammadisa226@gmail.com>
2025-05-12 23:22:27 +0700

    add multi language

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,7 +1,12 @@
+import { useTranslation } from "react-i18next";
 const ProjectsSection = () => {
+  const { t } = useTranslation();
   return (
     <div className="projects mt-32 py-10" id="projects">
-      <h1>Projects</h1>
+      <h1 className="text-center text-4xl font-bold mb-2 ">
+        {t("titleProject")}
+      </h1>
+      <p> {t("projectP1")}</p>
     </div>
   );
 };
