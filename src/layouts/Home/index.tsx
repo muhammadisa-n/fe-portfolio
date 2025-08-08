76dd6b46070b233837e1de0c5119b49245075eaa
muhammadisa-n <muhammadisa226@gmail.com>
2025-08-08 10:57:21 +0700

    fix api url

--- src/layouts/Home/index.tsx
+++ src/layouts/Home/index.tsx
@@ -6,8 +6,12 @@ const HomeSection = () => {
   const lang = i18n.language;
   const cvUrl =
     lang === "id"
-      ? `${import.meta.env.VITE_API_BASE_URL}/public/files/download/cv-ind`
-      : `${import.meta.env.VITE_API_BASE_URL}/public/files/download/cv-eng`;
+      ? `${
+          import.meta.env.VITE_API_BASE_URL
+        }/public/files/download/CV Resume Muhammad Isa - IND Version`
+      : `${
+          import.meta.env.VITE_API_BASE_URL
+        }/public/files/download/CV Resume Muhammad Isa - ENG Version`;
   return (
     <div
       className="hero grid md:grid-cols-2 pt-12 items-center xl:gap-0 gap-6 grid-cols-1"
