e7ece0e29f861f4a398dfb73a24cec9302290340
muhammadisa-n <muhammadisa226@gmail.com>
2026-05-13 21:21:54 +0700

    update deploy.yml catch error build

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,9 @@ import "./i18n";
 import "animate.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 AOS.init();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
