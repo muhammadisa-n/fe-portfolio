366d940cecc719a144c373139e07cf256f3c604d
muhammadisa-n <muhammadisa226@gmail.com>
2026-05-13 21:20:15 +0700

    update implement middleware app key for all routes

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,11 @@ import "./i18n";
 import "animate.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { Provider } from "react-redux";
-import store, { persistor } from "./redux/store";
-import { PersistGate } from "redux-persist/integration/react";
 AOS.init();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <div>
-          <App />
-        </div>
-      </PersistGate>
-    </Provider>
+    <div>
+      <App />
+    </div>
   </StrictMode>
 );
